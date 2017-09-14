b19b7ccdb3a2b87640bf93c81bb6a97163243ece
Santos <Micheli_C_Santos@AMER.DELL.COM>
2017-09-14 16:36:46 -0500

    first commit

---
+++ UnitTestProject/GeneralCode.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace UnitTestProject
+{
+    [Binding]
+    public class SearchPlace_ByDestinationAndPeriodSteps
+    {
+        IWebDriver Browser;
+
+        [BeforeScenario]
+        public void CreateWebDriver()
+        {
+            //Create a browser instance before execute the scenarios
+            this.Browser = new ChromeDriver();
+        }
+
+        [AfterScenario]
+        public void CloseWebDriver()
+        {
+            //Close the browser after finished the scenarios
+            this.Browser.Close();
+            this.Browser.Dispose();
+        }
+        
+        [Given(@"I am access the application")]
+        public void GivenIAmAccessTheApplication()
+        {
+            //Navigate to URL 
+            this.Browser.Navigate().GoToUrl("https://www.booking.com/index.html");
+        }
+        
+        [Given(@"I have opened Accommodations Page")]
+        public void GivenIHaveOpenedAccommodationsPage()
+        {
+            Assert.IsTrue(this.Browser.FindElement(By.XPath("//*[@id=\"cross-product-bar\"]/strong/span")).Text.Contains("Accommodations"));
+        }
+
+        [When(@"I search by field (.*)")]
+        public void WhenISearchByField(string destination)
+        {
+            this.Browser.FindElement(By.XPath("//*[@id=\"ss\"]")).SendKeys(destination);
+            this.Browser.FindElement(By.XPath("//*[@id=\"frm\"]/div[6]/div[2]/button/span[1]")).Click();
+        }
+        
+        [Then(@"the page returns record found")]
+        public void ThenThePageReturnsRecordFound()
+        {
+            Assert.IsTrue(this.Browser.FindElement(By.XPath("//*[@id=\"right\"]/div[3]/div/div/div/h1")).Text.Contains("properties found"));
+            
+        }
+    }
+}
---
+++ UnitTestProject/SearchPlace_ByDestination.feature
@@ -0,0 +1,15 @@
+﻿Feature: SearchPlace_ByDestination
+	In order to interact with the Booking.com application
+	As a user
+	I want to search a place by destination
+
+@mytag
+Scenario Outline: Search Place by Destination
+	Given I am access the application
+	And I have opened Accommodations Page
+	When I search by field <destination>
+	Then the page returns record found
+Examples: 
+	| destination     |
+	| New York City   |
+	| California, USA |
